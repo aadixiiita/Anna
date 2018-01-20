036458853e2ccb426817b81e9b8ec9b652cd7f7c
aadixiiita <34688457+aadixiiita@users.noreply.github.com>
2018-01-20 12:17:50 +0530

    Delete popup.js

--- js/popup.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $("#onoff-switch").on("change", function(e) {
-        a("onoff-switch", e.target.checked)
-    }),
-    $("#status-icon").on("change", function(e) {
-        a("status-icon", e.target.checked)
-    })
-});
